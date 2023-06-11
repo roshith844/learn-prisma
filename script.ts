ef552489edf596fce6e51bd3e2d77654ff83f583
ROSHITH P <roshith844@gmail.com>
2023-06-11 18:31:35 +0530

    chore: reformat and clean code

--- script.ts
+++ script.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 async function main() {
-  //   const user = await prisma.user.create({data: {name: 'kyle'}})
-  // await prisma.preferences.deleteMany()
-  // await prisma.user.deleteMany()
-  // const user = await prisma.user.createMany({
+
+  /* CREATE */
+  //  await prisma.user.create({data: {name: 'kyle'}}) // creates single data
+
+  // await prisma.user.createMany({  // creates many data
   //   data: [{
   //     userId: 1,
   //     name: 'roshithp',
@@ -25,23 +26,9 @@ async function main() {
   //     }
   // ]
   // })
-  // console.log(user)
-  // const output = await prisma.user.findMany({
-  //   where: {
 
-  //   }, 
-  // include: {
-  //   preferences: true
-  // }
-  // })
-  //  const out = await prisma.preferences.create({
-  //   data: {
-  //     id: 5,
-  //     emailUpdates: false,
-  //   }
-  //  })
-  //  console.log(out)
-  // const output = await prisma.user.findMany({
+  /* READ */
+  //  await prisma.user.findMany({
   //   where: {
   //     AND: [{ isAdmin: true, name: { not: 'me' } },
   //     { email: { notIn: ['spam@gmail.com', 'admin@gamil.com'], contains: '.com' } }]
@@ -51,7 +38,16 @@ async function main() {
   //     userId: "desc"
   //   }
   // })
-  // const output = await prisma.user.update({
+
+    // console.log(await prisma.preferences.findMany({
+  //   where: {
+  //     id: 5
+  //   },
+  //   include: { user: true }  // includes connected feild also
+  // }))
+
+  /* UPDATE */
+  //  await prisma.user.update({
   //   where: {
   //     : 'nazim@gmail.com'
   //   },
@@ -59,33 +55,19 @@ async function main() {
   //     email: 'nazim@mail.com'
   //   }
   // })
-  // const output = await prisma.user.update({
-  //   where: {
-  //     userId: 3
-  //   },
-  //   data: {
-  //     email: 'nazimAmeer@gmail.com'
-  //   }
-  // })
 
-  // console.log(output)
-  // const out = await prisma.preferences.update({
+  // await prisma.preferences.update({
   //   where: {
   //     id: 5
   //   },
   //   data: {
-  //   connect: {
+  //   connect: {  // connects with feild of userId from the another table 
   //     userId: 3
-  //   // }
+  //    }
   //   }
   // })
-  // console.log(out)
-  console.log(await prisma.preferences.findMany({
-    where: {
-      id: 5
-    },
-    include: { user: true }
-  }))
+
+
   // console.log(await prisma.user.findMany({
   //   where: {
   //     userId: 3
@@ -94,6 +76,7 @@ async function main() {
   //     preferences: true
   //   }
   // }))
+
   // console.log(await prisma.preferences.update({
   //   where: {
   //     id: 5
@@ -107,8 +90,10 @@ async function main() {
   //   }
   // }))
 
-
+  /* DELETE */
+  // await prisma.user.deleteMany()// deletes all data
 }
+
 main()
   .catch(e => {
     console.error(e.message)
